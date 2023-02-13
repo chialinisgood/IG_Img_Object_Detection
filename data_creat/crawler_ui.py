5efac7ddba73fcbad137b3d277533d0e3aaec6c7
Chialin_goood <97084679+chialinisgood@users.noreply.github.com>
2023-02-13 11:28:01 +0800

    Add files via upload

--- data_creat/crawler_ui.py
+++ data_creat/crawler_ui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from ig_img_download_test3 import Download_IG
 
 def openpicture(): 
@@ -9,39 +9,43 @@ def openpicture():
 
 def try_class():
     web = Download_IG(acc_e.get(),pwd_e.get(),save_e.get(),class_name.get())
-    web.run()
+    try:
+        web.run()
+        messagebox.showinfo(title='完成', message="圖片抓取完成")
+    except:
+        messagebox.showerror("爬取失敗",'請確認網域是否正常或帳號密碼是否正確')
 
 root=tk.Tk()
 root.title('IG_爬蟲')
 root.geometry('500x150')
 root.configure(bg='lightblue')
 root.iconbitmap('face2.ico')
+root.attributes("-topmost", True)
 
 acc = tk.Label(root,text='Account',bg='lightblue')
-acc.grid(row=0)
+acc.grid(row=0,padx=3,pady=3)
 pwd = tk.Label(root,text='Password',bg='lightblue')
-pwd.grid(row=1)
+pwd.grid(row=1,padx=3,pady=3)
 dir = tk.Label(root,text='Save Path',bg='lightblue')
-dir.grid(row=2)
+dir.grid(row=2,padx=3,pady=3)
 
 acc_e = tk.Entry(root,width=50)
-acc_e.grid(row=0,column=1) 
+acc_e.grid(row=0,column=1,padx=3,pady=3)
 pwd_e = tk.Entry(root,show='*',width=50)
-pwd_e.grid(row=1,column=1)
+pwd_e.grid(row=1,column=1,padx=3,pady=3)
 path = tk.StringVar()
 save_e = tk.Entry(root,textvariable=path,width=50)
-save_e.grid(row=2,column=1,sticky='w')
-dir_btn = tk.Button(root,text = '選擇路徑',command=openpicture).grid(row=2,column=2)
+save_e.grid(row=2,column=1,sticky='w',padx=3,pady=3)
+dir_btn = tk.Button(root,text = '選擇路徑',command=openpicture).grid(row=2,column=2,padx=3,pady=3)
 
 class_name = tk.StringVar()
 class_name.set(0)
 class_lab =tk.Label(root,bg='lightblue',font='微軟正黑體')
-class_lab.grid(row=3,column=0,columnspan=10)
-rb1 = tk.Radiobutton(class_lab,text='鳥',variable=class_name,value='bird',bg='lightblue').grid(row=4,column=1)
-rb1 = tk.Radiobutton(class_lab,text='貓',variable=class_name,value='cat',bg='lightblue').grid(row=4,column=2)
-rb1 = tk.Radiobutton(class_lab,text='狗',variable=class_name,value='dog',bg='lightblue').grid(row=4,column=3)
-rb1 = tk.Radiobutton(class_lab,text='馬',variable=class_name,value='horse',bg='lightblue').grid(row=4,column=4)
-
-login_btn = tk.Button(root,text = 'Start',command=try_class).grid(row=4,column=1)
+class_lab.grid(row=3,column=0,columnspan=10,padx=3)
+rb1 = tk.Radiobutton(class_lab,text='鳥',variable=class_name,value='bird',bg='lightblue').grid(row=4,column=1,padx=3,pady=3)
+rb1 = tk.Radiobutton(class_lab,text='貓',variable=class_name,value='cat',bg='lightblue').grid(row=4,column=2,padx=3,pady=3)
+rb1 = tk.Radiobutton(class_lab,text='狗',variable=class_name,value='dog',bg='lightblue').grid(row=4,column=3,padx=3,pady=3)
+rb1 = tk.Radiobutton(class_lab,text='馬',variable=class_name,value='horse',bg='lightblue').grid(row=4,column=4,padx=3,pady=3)
+login_btn = tk.Button(root,text = 'Start',command=try_class,activeforeground='#f00').grid(row=4,column=1)
 
 root.mainloop()
--- data_creat/ig_img_download_test3.py
+++ data_creat/ig_img_download_test3.py
@@ -8,8 +8,6 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import os
 import wget
-import random
-
 
 class Download_IG():
     def __init__(self,acc,pwd,save,category):
@@ -20,13 +18,15 @@ class Download_IG():
         
     def waits_web_login(self, ID, key):
         global driver
-        element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((ID, str(key))))
-        return element
+        try:
+            element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((ID, str(key))))
+            return element
+        except:
+            driver.quit()
 
     def web_driver(self):
         # acounts = ["chien_pingru",'chikichikichiachia','c8763_chien']
-        # acount = random.choice(acounts)
         options = webdriver.EdgeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         global driver
@@ -35,6 +35,7 @@ class Download_IG():
         driver.get("https://www.instagram.com/")
         username = self.waits_web_login(str(By.NAME),"username")
         password = self.waits_web_login(str(By.NAME),"password")
+
         login = driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[3]/button')
         username.clear()
         password.clear()
@@ -42,13 +43,13 @@ class Download_IG():
         password.send_keys(self.password)
         time.sleep(1)
         login.click()
+
         self.waits_web_login(By.XPATH,"//button[text()='稍後再說']").click()
         self.waits_web_login(By.XPATH,"//button[text()='稍後再說']").click()
-        # folderpath = r'C:\Users\Admin\Desktop\python\Independent_project\IG_imgs'
         folderpath = self.save_dir
         # keywords = ["#比基尼",'#泳裝','#比基尼泳裝']
-        keywords_dic = {'cat':["#貓",'#貓咪','#貓奴','cat'],'dog':["#狗",'#狗狗','#狗奴','#dog'],
-                        'bird':["#鳥",'#小鳥','#寵物鳥','bird'],'horse':["#馬",'#小馬','#horse', '#pony']}
+        keywords_dic = {'cat':["#貓",'#貓咪','#貓奴','#cat'],'dog':["#狗",'#狗狗','#狗奴','#dog'],
+                        'bird':["#鳥",'#小鳥','#寵物鳥','#bird'],'horse':["#馬",'#小馬','#horse', '#pony']}
         keywords = keywords_dic.get(self.cate)
         try:
             k = folderpath.split('/')[-1]
@@ -59,7 +60,6 @@ class Download_IG():
                 path = folderpath
         except:
             pass
-
         for keyword in keywords:
             self.waits_web_login(By.CLASS_NAME, "_aaw8").click()
             search = self.waits_web_login(By.CLASS_NAME, "_aauy")
@@ -68,21 +68,19 @@ class Download_IG():
             ActionChains(driver).send_keys(Keys.ENTER).perform()
             ActionChains(driver).send_keys(Keys.ENTER).perform()
             time.sleep(5)
-            
-            for i in range(5):
+            for i in range(3):
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 soup = BeautifulSoup(driver.page_source, 'html.parser')
                 img_line = soup.find_all('div','_aagv')
                 for imgs in img_line:
                     for img in imgs.select('img'):
-                        # print(img.get('src'))
                         url = img.get('src')
                         img_name = os.path.basename(url).split('?')[0].split('.')[0]+'.jpg'
                         save_img = os.path.join(path,img_name)
                         if not os.path.exists(save_img):
                             wget.download(url,save_img)
-                            # count+=1
                 time.sleep(5)
             driver.back()
+        driver.quit()
     def run(self):
         self.web_driver()
